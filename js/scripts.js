3c6692dc889563564bfb04581cb20ced8fab055c
Kacper Łukawski <lukawski.kacper@gmail.com>
2020-02-12 21:22:34 +0100

    Add temporary website

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // object-fit fix
+    var userAgent, ieReg, ie;
+    userAgent = window.navigator.userAgent;
+    ieReg = /msie|Trident.*rv[ :]*11\./gi;
+    ie = ieReg.test(userAgent);
+
+    if (ie) {
+        $(".ie-fit").each(function () {
+            var $container = $(this),
+                imgUrl = $container.find("img").prop("src");
+            if (imgUrl) {
+                $container.css("backgroundImage", 'url(' + imgUrl + ')').addClass("custom-object-fit");
+            }
+        });
+    }
+});
